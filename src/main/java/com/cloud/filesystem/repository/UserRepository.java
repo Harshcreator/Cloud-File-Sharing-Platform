52ef54421d899461cb23b8f535a5f9744098feff
Harshcreator <jayson22603@gmail.com>
2025-02-24 10:54:01 +0530

    User Repository Setup

--- src/main/java/com/cloud/filesystem/repository/UserRepository.java
+++ src/main/java/com/cloud/filesystem/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.cloud.filesystem.repository;
 
-public class UserRepository {
+import com.cloud.filesystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }

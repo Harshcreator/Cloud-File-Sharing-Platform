f836634e2b5fb601dc63068c3b9fdac70f808545
Harshcreator <jayson22603@gmail.com>
2025-02-24 10:55:49 +0530

    User Services Setup

--- src/main/java/com/cloud/filesystem/service/UserService.java
+++ src/main/java/com/cloud/filesystem/service/UserService.java
@@ -1,4 +1,19 @@
 package com.cloud.filesystem.service;
 
+import com.cloud.filesystem.entity.User;
+import com.cloud.filesystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
